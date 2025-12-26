21bc683a2fc071584fde1a922aa9a73d500fa59a
Syam-2211 <syamsona2009@gmail.com>
2025-12-26 13:47:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,92 @@
+//davidthegod
+
+const fs = require('fs')
+const { color } = require('./lib/myfunc')
+
+//owner
+global.owner = '919947121619'
+global.nomerowner = ["919947121619"]
+
+// Apikey 
+
+global.skizo = '🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓'
+global.casterix = '✪⃝⸸ꜛꪶ͢𝐒𝐘͔𝐀𝐌 𝐒𝐄𝐑͢ꫂꜜ™'
+//watermark 
+global.packname = '*✪⃝⸸ꜛꪶ͢𝐒𝐘͔𝐀𝐌 𝐒𝐄𝐑͢ꫂꜜ™*'
+global.author = '🤍⃞𝄟ꪶ𝐒͢ʏ᪳ᴀ͓ᴍ͎ ͢𝐒ᴇ͓ꪳʀ͎𖦻⃞🍓'
+
+// cpanel 
+global.domain = 'https://' // syam
+global.apikey2 = 'ptlc' // I love syam~ser
+global.capikey2 = 'ptla' // syam 
+global.eggsnya = '15' // The id eggs used if the id is 5, just leave it, don't change it
+global.location = '1' // id location
+
+
+global.apilinode = ''// apikey vps account linode
+global.apitokendo = ''
+
+//database 
+global.urldb = ''; // just leave it blank but if you want to use the mongo database, fill in the mongo url
+
+global.limitawal = {
+    premium: "Infinity",
+    monayawal: 0,
+    free: 100
+}
+
+//rpg
+global.buruan = {
+   ikan: 5,
+   ayam: 5,
+   kelinci: 5,
+   domba: 5,
+   sapi: 5,
+   gajah: 5
+}
+global.rpg = {
+   darahawal: 100,
+   energyawal: 252,
+   besiawal: 5,
+   goldawal: 1,
+   emeraldawal: 1,
+   umpanawal: 1,
+   potionawal: 1
+}
+
+//auto functioner
+global.autoTyping = false;                //make true to enable auto typing
+global.autoRecord = false;                //make true to enable auto recording
+global.autoViewStatus = false;       //make true to view statuses
+global.unavailable = true;     
+//to show your real presence  
+global.available = true;   
+// to show always online  
+global.autoreadmessages = false;   
+///To always read messages
+global.chatbot = false;
+global.autoreact = false;
+
+global.welcome = true;
+
+global.prefix = '.';
+
+global.autobio = false;
+
+global.antilink = false;
+
+global.antilinkkick = false;
+
+global.antilinkwarn = false;
+
+//_____________//
+// Don't change it
+global.antibot = false
+//—————「 Deadline 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(color(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
